1010981eccf5079a7b62d756cbdad7367904e7c4
pauloh-fm <phfarmar@gmail.com>
2022-10-22 22:45:14 -0300

    fix categoriasRepository.ts

--- src/repositories/categoriesRepository.ts
+++ src/repositories/categoriesRepository.ts
@@ -1,30 +1,28 @@
-// repositorio para mecher no banco de dados
-import { Category } from "../model/category";
-//DTO - data transfer objects - objeto para transferir dados
+// repositorio para manipular os dados
+import { Category } from '../model/category';
+// DTO - data transfer objects - objeto para transferir dados
 
 interface IcategoryDTO {
-    name: string;
-    description: string;
+  name: string;
+  description: string;
 }
 class CategoriesRepository {
-    private categories: Category[];
+  private categories: Category[];
 
-    constructor{
-        this.categories = [];
-    }
-    
-    create({description, name}: IcategoryDTO) : void{
+  constructor() {
+    this.categories = [];
+  }
 
-        const category = new Category(); // assim pra usar o constructor
-        Object.assign(category, {
-            name,
-            description,
-            created_at: new Date(),
-        });
+  create({ description, name }: IcategoryDTO): void {
+    const categorie = new Category(); // assim pra usar o constructor
+    Object.assign(categorie, {
+      name,
+      description,
+      created_at: new Date(),
+    });
 
-        this.categories.push(category);
-    }
-
+    this.categories.push(categorie);
+  }
 }
 
-export {CategoriesRepository}
+export { CategoriesRepository };
